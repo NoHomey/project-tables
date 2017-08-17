e2e4a466254ba8e7f428beb2995a7a284c7453ec
NoHomey <ivo.stratev.tues@gmail.com>
2017-08-17 17:40:20 +0300

    fixing open flags

--- src/FileIO/Writer/OutputFile/OutputFile.h
+++ src/FileIO/Writer/OutputFile/OutputFile.h
@@ -4,7 +4,7 @@
 
 #define ALL_READ_WRITE (S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH | S_IWOTH)
 
-#define WRITE_FLAGS (O_CREAT | O_DSYNC | O_SYNC | O_WRONLY | O_TRUNC) 
+#define WRITE_FLAGS (O_CREAT | O_SYNC | O_WRONLY | O_TRUNC) 
  
 class OutputFile {
 public:
